54d17d084404a184285ec7692d51a2766b8db950
IMing <hobnob848484@gmail.com>
2023-02-06 13:37:43 +0800

    fix: fix string encoding and add str test case

--- src/utils/utf8.ts
+++ src/utils/utf8.ts
@@ -20,41 +20,41 @@ export function utf8Encode(str: string, output: Uint8Array, outputOffset: number
 {
     const strLen = str.length;
     let offset = outputOffset;
-    for (let pos = 0; pos < strLen; pos++) {
-        let value = str.charCodeAt(pos);
+    let pos = 0;
+    while (pos < strLen) {
+        let value = str.charCodeAt(pos++);
         // 1-byte
         if ((value & 0xffffff80) == 0) {
             output[offset++] = value;
-            continue
-        }
-        // 2-byte
-        if ((value & 0xfffff800) == 0) {
-            output[offset++] = ((value >> 6) & 0x1f) | 0xc0;
             continue;
-        }
-
-        // handle surrogate pair
-        if (value >= 0xd800 && value <= 0xdbff)  {
-            // high surrogate
-            if (pos < strLen) {
-                const extra = str.charCodeAt(pos);
-                if ((extra & 0xfc00) === 0xdc00) {
-                    ++pos;
-                    value = ((value & 0x3ff) << 10) + (extra & 0x3ff) + 0x10000;
+        } else if ((value & 0xfffff800) == 0) {
+            // 2-byte
+            output[offset++] = ((value >> 6) & 0x1f) | 0xc0;
+        } else {
+            // handle surrogate pair
+            if (value >= 0xd800 && value <= 0xdbff)  {
+                // high surrogate
+                if (pos < strLen) {
+                    const extra = str.charCodeAt(pos);
+                    if ((extra & 0xfc00) === 0xdc00) {
+                        ++pos;
+                        value = ((value & 0x3ff) << 10) + (extra & 0x3ff) + 0x10000;
+                    }
                 }
+            }
+
+            // 3-byte
+            if ((value & 0xffff0000) === 0) {
+                output[offset++] = ((value >> 12) & 0x0f) | 0xe0;
+                output[offset++] = ((value >> 6) & 0x3f) | 0x80;
+            } else {
+                // 4-byte
+                output[offset++] = ((value >> 18) & 0x07) | 0xf0;
+                output[offset++] = ((value >> 12) & 0x3f) | 0x80;
+                output[offset++] = ((value >> 6) & 0x3f) | 0x80;
             }
         }
 
-        // 3-byte
-        if ((value & 0xffff0000) === 0) {
-            output[offset++] = ((value >> 12) & 0x0f) | 0xe0;
-            output[offset++] = ((value >> 6) & 0x3f) | 0x80;
-        } else {
-            // 4-byte
-            output[offset++] = ((value >> 18) & 0x07) | 0xf0;
-            output[offset++] = ((value >> 12) & 0x3f) | 0x80;
-            output[offset++] = ((value >> 6) & 0x3f) | 0x80;
-        }
         output[offset++] = (value & 0x3f) | 0x80;
     }
 }
